b9bf775a3080556ec8480bd7a247f2688ff5f690
http_Neitan <adiministrador0645@hotmail.com>
2021-03-14 17:48:06 -0300

    grupo de rotas auth-admin adicionado

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,14 +1,17 @@
 <?php
 
+
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::group(['middleware' => 'admin', 'prefix' => '/admin'], function () {
-    Route::get('/', 'AdminController@index')->name('admin.index');
-    Route::get('/dashboard', 'AdminController@dashboard')->name('admin.dashboard');
+    Route::group(['middleware' => 'auth-admin'], function () {
+        Route::get('/', 'AdminController@index')->name('admin.index');
+        Route::get('/dashboard', 'AdminController@dashboard')->name('admin.dashboard');
+    });
     Route::post('/login', 'AdminController@login')->name('admin.login');
     Route::get('/login', 'AdminController@formlogin')->name('admin.formlogin');
     Route::post('/register', 'AdminController@formRegister')->name('admin.formRegister');
-    Route::post('/logout', 'AdminController@logout')->name('admin.logout');
+    Route::get('/logout', 'AdminController@logout')->name('admin.logout');
 });
