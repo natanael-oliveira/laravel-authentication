3b345c95f836b176cfeed198ce51d321ffacb103
http_Neitan <adiministrador0645@hotmail.com>
2021-03-14 15:43:40 -0300

    added routes  admin

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,14 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::group(['middleware' => 'admin', 'prefix' => '/admin'], function () {
+    Route::get('/', 'AdminController@index')->name('admin.index');
+    Route::get('/dashboard', 'AdminController@dashboard')->name('admin.dashboard');
+    Route::post('/login', 'AdminController@login')->name('admin.login');
+    Route::get('/login', 'AdminController@formlogin')->name('admin.formlogin');
+    Route::post('/register', 'AdminController@formRegister')->name('admin.formRegister');
+    Route::post('/logout', 'AdminController@logout')->name('admin.logout');
 });
