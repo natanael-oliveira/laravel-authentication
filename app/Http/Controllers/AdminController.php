0081f5d53440acdb6875b876201da381aea5534d
http_Neitan <adiministrador0645@hotmail.com>
2021-03-15 15:12:16 -0300

    register configurado

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Admin;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\MessageBag;
 use Validator;
 
 class AdminController extends Controller
@@ -30,17 +33,40 @@ class AdminController extends Controller
             return redirect()
                 ->route('admin.login')
                 ->withErrors($validator)
-                ->withInput();
+                ->withInput(Input::except('password'));
         }
         if (auth()->guard('admin')->attempt($request->only('email', 'password'))) {
             return redirect()->route('admin.index');
         }
+        $errors = new MessageBag(['login' => ['Email and/or password invalid.']]);
+        return redirect()
+            ->route('admin.login')
+            ->withErrors($errors)
+            ->withInput(Input::except('password'));
     }
     public function formRegister()
     {
+        return view('admin.auth.register');
     }
     public function register(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|min:3|max:100',
+            'email' => 'required|min:3|max:100|unique:admins',
+            'password' => 'required|min:6|max:100|confirmed',
+            'password_confirmation' => 'required|min:3|max:100'
+        ]);
+        if ($validator->fails()) {
+            return redirect()
+                ->route('admin.register')
+                ->withErrors($validator)
+                ->withInput(Input::except('password'));
+        }
+        $admin = Admin::create($request->all());
+        if ($admin) {
+            auth()->guard('admin')->login($admin);
+            return redirect()->intended(route('admin.index'));
+        }
     }
     public function logout()
     {
